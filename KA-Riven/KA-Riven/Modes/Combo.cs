2475b2549a0b9eadfdc8bd0d3d0e98347f8154c3
Mario Gabriell <mariogk01@gmail.com>
2016-02-06 15:46:26 -0200

    Riven

---
+++ KA-Riven/KA-Riven/Modes/Combo.cs
@@ -0,0 +1,49 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+
+using Settings = KA_Riven.Config.Modes.Combo;
+
+namespace KA_Riven.Modes
+{
+    public sealed class Combo : ModeBase
+    {
+        public override bool ShouldBeExecuted()
+        {
+            return Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo);
+        }
+
+        public override void Execute()
+        {
+            var target = TargetSelector.GetTarget(Q.Range, DamageType.Magical);
+            if (target == null || target.IsZombie || target.HasUndyingBuff()) return;
+
+            if (Q.IsReady() && target.IsValidTarget(Q.Range) && Settings.UseQ && EventsManager.CanQ)
+            {
+                Q.Cast(target);
+            }
+            /*
+            if (E.IsReady() && target.IsValidTarget(E.Range) && Settings.UseE)
+            {
+                E.Cast(target);
+            }
+
+            if (W.IsReady() && target.IsValidTarget(W.Range) && Settings.UseW)
+            {
+                W.Cast();
+            }
+
+            if (R.IsReady() && Settings.UseR)
+            {
+                var targetR = TargetSelector.GetTarget(Q.Range + R.Range + 50, DamageType.Magical);
+
+                if (target.IsValidTarget(R.Range + Q.Range - 50) && targetR.CountEnemiesInRange(800) <= 2 &&
+                    Player.Instance.HealthPercent > targetR.HealthPercent && targetR.HealthPercent <= 50 ||
+                    targetR.Health < SpellDamage.GetTotalDamage(targetR))
+                {
+                    R.Cast(Player.Instance.Position.Extend(target.Position, R.Range + 250).To3D());
+                }
+            }
+            */
+        }
+    }
+}
---
+++ KA-Riven/KA-Riven/Modes/Harass.cs
@@ -0,0 +1,36 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+
+using Settings = KA_Riven.Config.Modes.Harass;
+
+namespace KA_Riven.Modes
+{
+    public sealed class Harass : ModeBase
+    {
+        public override bool ShouldBeExecuted()
+        {
+            return Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass);
+        }
+
+        public override void Execute()
+        {
+            var target = TargetSelector.GetTarget(Q.Range, DamageType.Physical);
+            if (target == null || target.IsZombie) return;
+
+            if (E.IsReady() && target.IsValidTarget(E.Range) && Settings.UseE)
+            {
+                E.Cast(target);
+            }
+
+            if (W.IsReady() && target.IsValidTarget(W.Range) && Settings.UseW)
+            {
+                W.Cast();
+            }
+
+            if (Q.IsReady() && target.IsValidTarget(Q.Range) && Settings.UseQ)
+            {
+                Q.Cast(target);
+            }
+        }
+    }
+}
