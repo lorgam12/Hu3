261118890cc583d8ec52d090d9cceb96ebb20f0e
Mario Gabriell <mariogk01@gmail.com>
2016-03-09 23:23:31 -0300

    HU3

---
+++ AutoHu3/AutoHu3/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutoHu3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutoHu3")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f25996f7-946a-41b8-9c63-e03704401838")]
+[assembly: AssemblyVersion("1")]
+[assembly: AssemblyFileVersion("1")]
---
+++ BottingMode/BottingMode/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+
+namespace BottingMode
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Loaded!");
+            Hacks.DisableTextures = true;
+            Console.WriteLine(Hacks.DisableTextures);
+        }
+    }
+}
