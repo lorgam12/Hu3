6ec399dc38b93f34a274c51241ddfbae40d38a30
Mario Gabriell <mariogk01@gmail.com>
2016-04-16 21:23:23 -0300

    Update

---
+++ URF Spell Spammer/URF Spell Spammer/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("URF Spell Spammer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("URF Spell Spammer")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3cf34cbd-a448-4a32-a125-20be769a5e50")]
+
+[assembly: AssemblyVersion("4")]
+[assembly: AssemblyFileVersion("4")]
